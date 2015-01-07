85c664dc591df0bd8eefe403165b090272144e58
Quinn Hoyer <qhoyer@practicefusion.com>
2015-01-07 14:07:30 -0800

    Inital Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var Writer = require('broccoli-writer')
+  , pp = require('preprocess')
+  , path = require('path');
+
+function PreprocessFilter (inputTree, options) {
+  if (!(this instanceof PreprocessFilter)) {
+    return new PreprocessFilter(inputTree, options);
+  }
+
+  this.inputTree = inputTree;
+  this.options = options;
+
+}
+
+PreprocessFilter.prototype = Object.create(Writer.prototype);
+PreprocessFilter.prototype.constructor = PreprocessFilter;
+
+PreprocessFilter.prototype.write = function (readTree, destDir) {
+  var _this = this;
+
+  return readTree(this.inputTree).then(function (result) {
+    var src = path.join(result, _this.options.src)
+      , dest = path.join(destDir, _this.options.dest)
+
+    pp.preprocessFileSync(src, dest, _this.options.options.context);
+  });
+};
+
+module.exports = PreprocessFilter;
